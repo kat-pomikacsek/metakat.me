225e8513f9717c9ba5cb445e3b3c624098de9bb3
Richard Bausek <office@artibella.com>
2022-03-06 20:38:19 +0100

    Tweak design of portfolio Upgrade to Tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    'src/**/*.{njk,js,css}'
+  ],
+  safelist: [
+    {
+      pattern: /(text|decoration|bg|border)-(petrol|sky|mango|peach)/,
+      variants: ['sm', 'md', 'lg', 'hover']
+    },
+    {
+      pattern: /(text|decoration|bg|border)-(petrol|sky|mango|peach)-(light|lightest|gray|dark|bright)/,
+      variants: ['sm', 'md', 'lg', 'hover']
+    },
+    // theme background gradients
+    {
+      pattern: /split-white-(petrol|sky|mango|peach)/,
+      variants: ['sm', 'md', 'lg', 'hover']
+    },
+    {
+      pattern: /split-(petrol|sky|mango|peach)-white/,
+      variants: ['sm', 'md', 'lg', 'hover']
+    },
+  ]
+  ,
   theme: {
     colors: {
       transparent: 'transparent',
@@ -16,6 +37,7 @@ module.exports = {
         lightest: '#ECF7F8',
         DEFAULT: '#ADDFE2',
         dark: '#198B92',
+        bright: '#5BE2EB',
         gray: '#FBFBFB'
       },
       sky: {
@@ -23,6 +45,7 @@ module.exports = {
         lightest: '#F5FAFD',
         DEFAULT: '#C7E3F0',
         dark: '#23789F',
+        bright: '#60CFFF',
         gray: '#FBFBFB'
       },
       mango: {
@@ -30,6 +53,7 @@ module.exports = {
         lightest: '#FFFAE9',
         DEFAULT: '#FFEAB6',
         dark: '#D49A01',
+        bright: '#FFD25E',
         gray: '#FBFBFB'
       },
       peach: {
@@ -37,6 +61,7 @@ module.exports = {
         lightest: '#FFF0E8',
         DEFAULT: '#FFC09C',
         dark: '#E05101',
+        bright: '#FFA775',
         gray: '#FBFBFB'
       },
       gray: {
@@ -46,17 +71,6 @@ module.exports = {
         lightest: '#FBFBFB'
       }
     },
-    backgroundImage: {
-      'split-white-petrol': "linear-gradient(to bottom, white 20%, #ADDFE2 20%);",
-      // 'split-white-sky': "linear-gradient(to bottom, white 20%, #C7E3F0 20%);",
-      'split-white-sky': "linear-gradient(to bottom, white 20%, rgba(255,255,255,0) 20%), radial-gradient(40% 100% at 70% 26.94%, rgba(112, 212, 255, 0.357) 0%, rgba(112, 212, 255, 0.7) 100%);",
-      'split-white-mango': "linear-gradient(to bottom, white 20%, #FFEAB6 20%);",
-      'split-white-peach': "linear-gradient(to bottom, white 20%, #FFC09C 20%);",
-      'split-petrol-white': "linear-gradient(to bottom, #ADDFE2 80%, white 80%);",
-      'split-sky-white': "linear-gradient(to bottom, #C7E3F0 80%, white 80%);",
-      'split-mango-white': "linear-gradient(to bottom, #FFEAB6 80%, white 80%);",
-      'split-mango-white': "linear-gradient(to bottom, #FFC09C 80%, white 80%);"
-    },
     fontFamily: {
       sans: ['Overpass', 'sans-serif']
     },
@@ -64,7 +78,7 @@ module.exports = {
       maxWidth: {
         'section': '720px',
         'header': '820px',
-        'overview': '1728px',
+        'overview': '1536px',
         'screen-desktop': '38.5rem', //616px
         'screen-mobile': '9.5rem', //152px
         'screens': '820px'
@@ -78,17 +92,20 @@ module.exports = {
       },
       screens: {
         'sm': '640px',  
-        'md': '768px', // 48rem 
+        'md': '832px', // 52rem 
         'lg': '1024px', // 64rem
         'xl': '1280px',  
-        '2xl': '1728px',
+        '2xl': '1536px',
       },
       fontSize: {
-        'xl': ['1.25rem', '2rem'],
-        '2xl': ['1.563rem', '2rem'],
-        '3xl': ['2rem', '2.7rem'],
-        '4xl': ['2.5rem', '3.375rem'],
-        '6xl': ['3.75rem', '4.5rem'],
+        'sm': ['0.875rem', '1.3rem'],
+        'base': ['1rem', '1.5rem'],
+        'xl': ['1.25rem', '1.875rem'],
+        '2xl': ['1.5rem', '1.95rem'],
+        '3xl': ['2.185rem', '2.615rem'],
+        '4xl': ['2.5rem', '2.875rem'],
+        '5xl': ['3.125rem', '3.595rem'],
+        '6xl': ['3.75rem', '4.125rem'],
 
       },
       padding: {
@@ -100,15 +117,19 @@ module.exports = {
       },
       gap: {
         '13': '3.25rem' // 52px
-       }      
+       },
+      backgroundImage: {
+        'split-white-petrol': "linear-gradient(to bottom, white 16%, rgba(255,255,255,0) 16%), radial-gradient(60% 97% at 30% 0%, #ADDFE2 0%, #5BE2EB 100%);",
+        'split-white-sky': "linear-gradient(to bottom, white 16%, rgba(255,255,255,0) 16%), radial-gradient(60% 97% at 72% 0%, #C7E3F0 0%, #60CFFF 84%);",
+        'split-white-mango': "linear-gradient(to bottom, white 16%, rgba(255,255,255,0) 16%), radial-gradient(60% 97% at 72% 0%, #FFEAB6 0%, #FFD25E 100%);",
+        'split-white-peach': "linear-gradient(to bottom, white 16%, rgba(255,255,255,0) 16%), radial-gradient(60% 97% at 36% 0%, #FFC09C 0%, #FFA775 100%);",
+        'split-petrol-white': "linear-gradient(0deg, #FFFFFF 16%, rgba(255, 255, 255, 0) 16%), radial-gradient(60% 97% at 30% 0%, #ADDFE2 0%, #5BE2EB 100%);",
+        'split-sky-white': "radial-gradient(60% 97% at 72% 0%, #C7E3F0 0%, #60CFFF 100%), linear-gradient(to bottom, white 84%, rgba(255,255,255,0) 84%);",
+        'split-mango-white': "linear-gradient(to bottom, #FFEAB6 80%, white 80%);",
+        'split-mango-white': "linear-gradient(to bottom, #FFC09C 80%, white 80%);"
+      },      
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-  purge: process.env.ELEVENTY_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
+
 }
